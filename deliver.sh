76a0e83dc4fa11a729a17e8356306c5be27a77f3
OlehKovalchyn <49445002+OlehKovalchyn@users.noreply.github.com>
2019-06-19 11:10:05 +0300

    Update deliver.sh

--- deliver.sh
+++ deliver.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/env bash
-sudo -i
+set +x
 mvn jar:jar install:install help:evaluate -Dexpression=project.name
-
+set -x
+set +x
 NAME=`mvn help:evaluate -Dexpression=project.name | grep "^[^\[]"`
-
+set -x
+set +x
 VERSION=`mvn help:evaluate -Dexpression=project.version | grep "^[^\[]"`
-
+set -x
+set +x
 java -jar target/${NAME}-${VERSION}.jar
+set -x
